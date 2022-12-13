02c21c149d81cea7175de77cb6b41f1f66b5d622
Divy Srivastava <dj.srivastava23@gmail.com>
2022-12-13 08:53:43 +0530

    License checker

--- build.ts
+++ build.ts
@@ -1,5 +1,22 @@
 #!/usr/bin/env deno run -A --unstable
 
+// Copyright (c) 2022 Divy Srivastava.
+//
+// This file is part of elsaland/elsa.
+// See https://github.com/elsaland/elsa-next for further info.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import $ from "https://deno.land/x/dax/mod.ts";
 import { checkLicense } from "https://deno.land/x/license_checker@v3.1.4/lib.ts";
 
@@ -27,7 +44,7 @@ async function release() {
 
 const years = "2022";
 const owner = "Divy Srivastava";
-const projectname = "elsa";
+const projectname = "elsaland/elsa";
 const projecturl = "https://github.com/elsaland/elsa-next";
 
 async function checkApache2(fix: boolean) {
@@ -41,23 +58,22 @@ async function checkApache2(fix: boolean) {
         config: [
           [
             "**/*.{js,ts,jsx,tsx,rs}",
-            `
-Copyright (c) ${years} ${owner}.
-
-This file is part of ${projectname}.
-See ${projecturl} for further info.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+            `// Copyright (c) ${years} ${owner}.
+//
+// This file is part of ${projectname}.
+// See ${projecturl} for further info.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 `,
           ],
         ],
